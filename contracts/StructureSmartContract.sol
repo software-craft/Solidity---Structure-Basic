fee41d04d97a8cd4434542a1c0196a2dbbfe5350
Leonardogit <leonardoiglesias74@gmail.com>
2026-02-02 09:19:26 -0300

    testcases

--- contracts/StructureSmartContract.sol
+++ contracts/StructureSmartContract.sol
@@ -1,76 +1,66 @@
 // License
-    // SPDX-License-Identifier: LGPL-3.0-only
+// SPDX-License-Identifier: LGPL-3.0-only
 
 // Version Solidity
-
 pragma solidity 0.8.30;
 
 // Contract
-
 contract Structure {
-
-// Numbers
-
-// [0 - 2^256-1]
-uint256 number1 = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
-
-uint16 number2 = 256; // [0 - 2^256-1]
-
-uint8 number3 = 127; // [-2^7 - 2^7-1]
-
-uint32 result = number2 * number3;
-
-// String
-
-string word = "Happy 2026";
-
-// Boleans
-
-bool test = true;
-bool test1 = false;
-
-// Array
-
-uint256[5] arraytest = [1, 2, 3, 4, 5];
-
-
-// Mapping
-
-mapping (address => uint) name;(address => uint256) example;
-
-struct Person {
-
-    string name;
-    uint256 age;
-}
-
-// Modifiers
-
-modifier modifier1 {
-
-    if (5 + 2 != 10) revert();
-    _;
-}
-
-// Events
-
-event Multiplier(uint256 result);
-
-// Functions
-
-// External
-// word (function) + name + arguments + visibility + modifiers + return value
-// Visibility:
-    // - external
-    // - internal 
-    // - public
-    // - private
-
-function multiplier(uint256 number1_, uint256 number2_) public modifier1 returns (uint256 result_) {
-    result_ = number1_ * number2_;
-
-    emit Multiplier(result_);
-    return result_;
-
-
+    // Numbers
+    // [0 - 2^256-1]
+    uint256 public number1 = type(uint256).max;
+    
+    uint16 public number2 = 256; // [0 - 65535]
+    
+    uint8 public number3 = 127; // [0 - 255]
+    
+    uint32 public result;
+    
+    // String
+    string public word = "Happy 2026";
+    
+    // Booleans
+    bool public test = true;
+    bool public test1 = false;
+    
+    // Array
+    uint256[5] public arraytest = [uint256(1), 2, 3, 4, 5];
+    
+    // Mappings
+    mapping(address => uint256) public name;
+    mapping(address => uint256) public example;
+    
+    // Struct
+    struct Person {
+        string name;
+        uint256 age;
+    }
+    
+    Person public person;
+    
+    // Modifiers
+    modifier modifier1() {
+        require(5 + 2 == 3, "Condition failed");
+        _;
+    }
+    
+    // Events
+    event Multiplier(uint256 result);
+    
+    // Constructor
+    constructor() {
+        result = uint32(uint256(number2) * uint256(number3));
+        person = Person("Default", 0);
+    }
+    
+    // Functions
+    function multiplier(uint256 number1_, uint256 number2_) 
+        public 
+        modifier1 
+        returns (uint256 result_) 
+    {
+        result_ = number1_ * number2_;
+        emit Multiplier(result_);
+        return result_;
+    }
 }
